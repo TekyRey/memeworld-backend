b49d8174685fcdf0cd68bfbd97116dff51801396
TekyRey <mwakabayah@gmail.com>
2021-11-11 20:27:49 +0300

    added posts controller

--- api/posts/post.controller.js
+++ api/posts/post.controller.js
@@ -4,6 +4,11 @@ const {
     getCategoryById,
     updateCategory,
     deleteCategory,
+    createPost,
+    getPosts,
+    getPostById,
+    updatePost,
+    deletePost,
   } = require("./post.service");
   const { insertCategories, updateCategories } = require("../../schema/postsSchema");
   module.exports = {
@@ -140,8 +145,136 @@ const {
       });
     },
   
-   
-  
-   
+   //posts controllers
+
+    // create post controller
+    createPostController: (req, res) => {
+        const body = req.body;
+        const validationResult = insertPosts.validate(body);
+        if (validationResult.error) {
+          // 400 bad request
+          return res.status(400).send(validationResult.error.details[0].message);
+        }
+    
+        
+        createPost(body, (err, results) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            return res.status(500).json({
+              errorCode: 500,
+              errorMessage: "Internal Server Error",
+            });
+          }
+          return res.status(200).json({
+            success: 1,
+            data: results,
+          });
+        });
+      },
+    
+      //getPostById controller
+      getPostByIdController: (req, res) => {
+        const id = req.params.id;
+        getPostById(id, (err, results) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            return res.status(500).json({
+              errorCode: 500,
+              errorMessage: "Internal Server Error",
+            });
+          }
+          if (!results[0]) {
+            return res.status(204).json({
+              errorCode: 204,
+              errorMessage: "Record not found",
+              // will not be returned anyways...
+            });
+          }
+          return res.json({
+            success: 1,
+            data: results[0],
+          });
+        });
+      },
+    
+      // get posts controller
+      getPostsController: (req, res) => {
+        getPosts((err, results) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            return res.status(500).json({
+              errorCode: 500,
+              errorMessage: "Internal Server Error",
+            });
+          }
+          if (!results) {
+            return res.status(204).json({
+              errorCode: 204,
+              errorMessage: "Records not found",
+              // will not be returned anyways
+            });
+          }
+          return res.json({
+            success: 1,
+            data: results,
+          });
+        });
+      },
+    
+      // update post controller
+      updatePostController: (req, res) => {
+        const body = req.body;
+        const validationResult = updatePosts.validate(body);
+        if (validationResult.error) {
+          // 400 bad request
+          return res.status(400).send(validationResult.error.details[0].message);
+        }
+       
+    
+        updatePost(body, (err, results) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            return res.status(500).json({
+              errorCode: 500,
+              errorMessage: "Internal Server Error",
+            });
+          }
+          if (!results.affectedRows) {
+            return res.status(501).json({
+              errorCode: 501,
+              errorMessage: "Not Implemented",
+            });
+          }
+          return res.json({
+            success: 1,
+            message: "updated successfully",
+          });
+        });
+      },
+    
+      // delete post controller
+      deletePostController: (req, res) => {
+        const data = req.body;
+        deletePost(data, (err, results) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            return res.status(500).json({
+              errorCode: 500,
+              errorMessage: "Internal Server Error",
+            });
+          }
+          if (!results) {
+            return res.status(204).json({
+              errorCode: 204,
+              errorMessage: "Record not found",
+            });
+          }
+          return res.json({
+            success: 1,
+            message: "record deleted successfuly",
+          });
+        });
+      },
+    
   };
   
