a0ec331118af8c7285f0240de1f1f8fb403426af
TekyRey <mwakabayah@gmail.com>
2021-11-11 20:16:51 +0300

    added posts service

--- api/posts/post.service.js
+++ api/posts/post.service.js
@@ -74,6 +74,82 @@ module.exports = {
     );
   },
 
+  //posts service
+
+  createPost: (data, callback) => {
+    pool.query(`insert into posts SET ?`, data, (error, results) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, results);
+    });
+  },
+
+  getPosts: (callback) => {
+    pool.query(`select * from posts`, [], (error, results) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, results);
+    });
+  },
+
+  getPostById: (id, callback) => {
+    pool.query(
+      `select * from posts where id =?`,
+      [id],
+      (error, results) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+
+  updatePost: (data, callback) => {
+    // get the query keys present in the data object
+    const queryKeysArray = Object.keys(data);
+
+    // get the query values present in the data object
+    const queryValuesArray = Object.values(data);
+
+    // form update query string from queryKeysArray
+    let queryString = "";
+    queryKeysArray.map((item) => (queryString += item + "= ?,"));
+
+    // add where clause to query string(after removing the trailing comma)
+    queryString = queryString.replace(/,\s*$/, "");
+    queryString = `${queryString} WHERE id = ? `;
+
+    queryValuesArray.push(data.id); // adds categoryid at the end
+
+    pool.query(
+      `UPDATE posts SET ${queryString}`,
+      queryValuesArray,
+      (error, results) => {
+        if (error) {
+          callback(error);
+        }
+
+        return callback(null, results);
+      }
+    );
+  },
+
+  deletePost: (data, callback) => {
+    pool.query(
+      `delete from posts where id = ?`,
+      [data.id],
+      (err, results) => {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+
 
  
 };
