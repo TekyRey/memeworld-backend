9c460ea67c8c009f26bf2ffb0ec772bf191bb831
TekyRey <mwakabayah@gmail.com>
2021-11-13 09:10:34 +0300

    added post favorites service

--- api/posts/post.service.js
+++ api/posts/post.service.js
@@ -226,5 +226,81 @@ module.exports = {
     );
   },
 
+   //posts favorites service
+
+   createPostFavorite: (data, callback) => {
+    pool.query(`insert into post_favorites SET ?`, data, (error, results) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, results);
+    });
+  },
+
+  getPostFavorites: (callback) => {
+    pool.query(`select * from post_favorites`, [], (error, results) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, results);
+    });
+  },
+
+  getPostFavoriteByUserId: (user_id, callback) => {
+    pool.query(
+      `select * from post_favorites where user_id =?`,
+      [user_id],
+      (error, results) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+
+  updatePostFavorite: (data, callback) => {
+    // get the query keys present in the data object
+    const queryKeysArray = Object.keys(data);
+
+    // get the query values present in the data object
+    const queryValuesArray = Object.values(data);
+
+    // form update query string from queryKeysArray
+    let queryString = "";
+    queryKeysArray.map((item) => (queryString += item + "= ?,"));
+
+    // add where clause to query string(after removing the trailing comma)
+    queryString = queryString.replace(/,\s*$/, "");
+    queryString = `${queryString} WHERE user_id = ? `;
+
+    queryValuesArray.push(data.id); // adds categoryid at the end
+
+    pool.query(
+      `UPDATE post_favorites SET ${queryString}`,
+      queryValuesArray,
+      (error, results) => {
+        if (error) {
+          callback(error);
+        }
+
+        return callback(null, results);
+      }
+    );
+  },
+
+  deletePostFavorite: (data, callback) => {
+    pool.query(
+      `delete from post_favorites where user_id = ?`,
+      [data.user_id],
+      (err, results) => {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+
 
 };
