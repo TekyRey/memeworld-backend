e5eac8cd995914404cd7c329a068ab18a8945abb
TekyRey <mwakabayah@gmail.com>
2021-11-13 08:25:58 +0300

    added postlikes router and schema

--- api/posts/post.router.js
+++ api/posts/post.router.js
@@ -9,11 +9,18 @@ const {
     getPostByIdController,
     updatePostController,
     deletePostController,
+    createPostLikeController,
+    getPostLikeByUserIdController,
+    getPostLikesController,
+    updatePostLikeController,
+    deletePostLikeController,
   } = require("./post.controller");
   const router = require("express").Router();
   const { verifyToken } = require("../../auth/token_validation");
   
-   //posts routes
+
+ 
+ //posts routes
    router.post("/", verifyToken(), createPostController);
    router.get("/", verifyToken(), getPostsController);
    router.get("/:id",verifyToken(),getPostByIdController);
@@ -26,6 +33,13 @@ const {
   router.get("categories/:id",verifyToken(),getCategoryByIdController);
   router.patch("/categories",verifyToken(),updateCategoryController);
   router.delete("/categories",verifyToken(), deleteCategoryController);
+
+   //post likes routes
+   router.post("/postlikes", verifyToken(), createPostLikeController);
+   router.get("/postlikes", verifyToken(), getPostLikeByUserIdController);
+   router.get("/postlikes:id",verifyToken(),getPostLikesController);
+   router.patch("/postlikes",verifyToken(),updatePostLikeController);
+   router.delete("/postlikes",verifyToken(), deletePostLikeController);
 
  
   module.exports = router;
